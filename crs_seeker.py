f5f9dbcf727939012d6b77296a2535895d1dabb6
Marshblocker <glmarinas@up.edu.ph>
2021-12-28 21:32:09 +0800

    Reduce code width

--- crs_seeker.py
+++ crs_seeker.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 from bs4.element import ResultSet, Tag
 from typing import NewType
+from os import system, name
 
 
 Chance = float # Percentage probability of being chosen for the class.
@@ -8,6 +9,9 @@ ClassesType = NewType('ClassesType', list[tuple[ResultSet[Tag], Chance]])
 
 allowed_restrictions = ['DCS', 'BS CS'] # Modify if needed.
 
+
+def clear_screen():
+    system('cls' if name == 'nt' else 'clear')
 
 def add_to_classes(classes: ClassesType, class_fields: ResultSet[Tag]):
     assert isinstance(class_fields[5].b, Tag), f'Not tag.'
@@ -32,13 +36,15 @@ def main() -> None:
     html_lines: list[str] = []
 
     # Convert HTML input into a BeautifulSoup.
-    print('Input the source code of the webpage of the chosen preenlistment class list\n(Can be viewed by pressing Ctrl+U): ')
+    print('Input the source code of the webpage of the chosen preenlistment class list\n' + 
+          '(Can be viewed by pressing Ctrl+U on the target webpage): ')
     while True:
         line = input()
 
         html_lines.append(line) 
         if line == '</html>':
             break
+    clear_screen() 
 
     html: str = '\n'.join(html_lines)
     s = bs(html, 'html.parser')
@@ -79,10 +85,14 @@ def main() -> None:
     classes.sort(key= lambda x: x[1], reverse= True)
 
     # Print classes with the highest chances.
-    print('\nTop available classes based on chance of acquiring a slot (Note: some classes may actually not be available, check the Remarks Section):\n')
-    print('╔' + '═' * 5 + '╦' + '═' * 12 + '╦' + '═' * 28 + '╦' + '═' * 29 + '╦' + '═' * 55 + '╦' + '═' * 10 + '╗')
-    print('║  N  ║ Class Code ║        Instructor/s        ║        Schedule/Room        ║                        Remarks                        ║  Chance  ║')
-    print('╠' + '═' * 5 + '╬' + '═' * 12 + '╬' + '═' * 28 + '╬' + '═' * 29 + '╬' + '═' * 55 + '╬' + '═' * 10 + '╣')
+    print('\nTop available classes based on chance of acquiring a slot' + 
+          '(Note: some classes may actually not be available, check the Remarks Section):\n')
+    print('╔' + '═' * 5 + '╦' + '═' * 12 + '╦' + '═' * 28 + '╦' + '═' * 29 + 
+          '╦' + '═' * 55 + '╦' + '═' * 10 + '╗')
+    print('║  N  ║ Class Code ║        Instructor/s        ║        Schedule/Room' + 
+          '        ║                        Remarks                        ║  Chance  ║')
+    print('╠' + '═' * 5 + '╬' + '═' * 12 + '╬' + '═' * 28 + '╬' + '═' * 29 + 
+          '╬' + '═' * 55 + '╬' + '═' * 10 + '╣')
     for i in range(nclasses):
         # If the class contains both a lab and lecture component, print only the lab component.
         class_code = classes[i][0][0].get_text('$').split('$')[0]
@@ -100,11 +110,14 @@ def main() -> None:
             remarks = 'None'
 
         chance_ = str(classes[i][1]) + ' %'
-        print(f'║{str(i+1).center(5)}║{class_code.center(12)}║{instructor.center(28)}║{schedroom.center(29)}║{remarks.center(55)}║{chance_.center(10)}║')
+        print(f'║{str(i+1).center(5)}║{class_code.center(12)}║{instructor.center(28)}║' +
+              f'{schedroom.center(29)}║{remarks.center(55)}║{chance_.center(10)}║')
         if i != nclasses - 1:
-            print('║' + '-' * 5 + '║' + '-' * 12 + '║' + '-' * 28 + '║' + '-' * 29 + '║' + '-' * 55 + '║' + '-' * 10 + '║')
+            print('║' + '-' * 5 + '║' + '-' * 12 + '║' + '-' * 28 + '║' + 
+                  '-' * 29 + '║' + '-' * 55 + '║' + '-' * 10 + '║')
 
-    print('╚' + '═' * 5 + '╩' + '═' * 12 + '╩' + '═' * 28 + '╩' + '═' * 29 + '╩' + '═' * 55 + '╩' + '═' * 10 + '╝')   
+    print('╚' + '═' * 5 + '╩' + '═' * 12 + '╩' + '═' * 28 + '╩' + '═' * 29 + 
+          '╩' + '═' * 55 + '╩' + '═' * 10 + '╝')   
 
 
 if __name__ == '__main__':
